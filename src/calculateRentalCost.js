340811dbddfdaeeb677331775f6e59e675095f2c
guilherme <guiribeiro2322@gmail.com>
2025-12-06 22:06:43 -0300

    enviando function

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -5,6 +5,16 @@
  */
 function calculateRentalCost(days) {
   // write code here
+  const amountDay = 40
+  const totalDays = days
+  const firstDisc = 50
+  const secDisc = 20
+
+  const totalAmount = amountDay * totalDays
+
+  if(totalDays >= 7) return totalAmount - firstDisc
+  if(totalDays >= 3 || totalAmount < 7) return totalAmount - secDisc
+  return totalAmount
 }
 
 module.exports = calculateRentalCost;
